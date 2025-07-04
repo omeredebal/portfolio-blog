b56375d1983d171c9b9d4490299268e2ae61b7c3
Ömer Edebalı <omeredebal@hotmail.com>
2025-07-04 09:04:51 +0300

    Projects, Blog ve Contact sayfaları eklendi

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function BlogPage() {
+  return <h2 className="text-2xl font-semibold">Yazılarım</h2>;
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function ContactPage() {
+  return <h2 className="text-2xl font-semibold">İletişim</h2>;
+}
